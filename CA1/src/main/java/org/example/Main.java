a6e1fdfd96bf2fddabb8c0c196666dd47f4d2ed1
mvajhi <mvajhimv@gmail.com>
2025-02-18 22:40:56 +0330

    Feat: read & write data in start and end of the program

--- CA1/src/main/java/org/example/Main.java
+++ CA1/src/main/java/org/example/Main.java
@@ -1,17 +1,37 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Hotel hotel = new Hotel();
+        readDataFromFile(hotel);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Perform operations on hotel object if needed
+
+        writeDataInFile(hotel);
+    }
+
+    private static void writeDataInFile(Hotel hotel) {
+        try {
+            String stateJson = hotel.logState();
+            java.nio.file.Files.write(new File("src/main/resources/state.json").toPath(), stateJson.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readDataFromFile(Hotel hotel) {
+        try {
+            String jsonData = new String(java.nio.file.Files.readAllBytes(new File("src/main/resources/data.json").toPath()));
+            hotel.parseJson(jsonData);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
