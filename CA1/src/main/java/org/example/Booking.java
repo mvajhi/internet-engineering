f8b02d73a6d6c99f34b59fb6c8448bb3cea23a48
mvajhi <mvajhimv@gmail.com>
2025-02-18 22:25:37 +0330

    Feat: check in & out time and throw exception

--- CA1/src/main/java/org/example/Booking.java
+++ CA1/src/main/java/org/example/Booking.java
@@ -10,12 +10,15 @@ public class Booking {
     private LocalDateTime checkIn;
     private LocalDateTime checkOut;
 
-    public Booking(int ID, Room room, Customer customer, LocalDateTime checkIn, LocalDateTime checkOut) {
+    public Booking(int ID, Room room, Customer customer, LocalDateTime checkIn, LocalDateTime checkOut) throws IllegalArgumentException {
         this.ID = ID;
         this.room = room;
         this.customer = customer;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        if (checkOut.isBefore(checkIn)) {
+            throw new IllegalArgumentException("Check-out date must be after check-in date");
+        }
     }
 
     public int getID() {
