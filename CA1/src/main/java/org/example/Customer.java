a7d2efa1f9d061b61f627aab132c934b09132e54
mvajhi <mvajhimv@gmail.com>
2025-02-18 17:05:30 +0330

    Feat: Added JsonProperty to map Json to class

--- CA1/src/main/java/org/example/Customer.java
+++ CA1/src/main/java/org/example/Customer.java
@@ -1,10 +1,19 @@
 package org.example;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer {
+    @JsonProperty("ssn")
     public int NID;
+    @JsonProperty("name")
     public String name;
+    @JsonProperty("phone")
     public String phone;
+    @JsonProperty("age")
     public int age;
+
+    public Customer() {
+    }
 
     public Customer(int NID, String name, String phone, int age) {
         this.NID = NID;
--- CA1/src/main/java/org/example/Room.java
+++ CA1/src/main/java/org/example/Room.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Room {
+    @JsonProperty("id")
     public int Number;
+    @JsonProperty("capacity")
     public int Capacity;
+
+    public Room() {
+    }
 
     public Room(int Number, int Capacity) {
         this.Number = Number;
