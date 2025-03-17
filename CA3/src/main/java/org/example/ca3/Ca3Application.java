7216a1bbea13bad3037316e5a138d6d92cd88132
mvajhi <mvajhimv@gmail.com>
2025-03-17 12:32:06 +0330

    Feat: Start CA3

---
+++ CA3/src/main/java/org/example/ca3/Ca3Application.java
@@ -0,0 +1,13 @@
+package org.example.ca3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ca3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ca3Application.class, args);
+    }
+
+}
