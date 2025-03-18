00c8eb4d353c394638ca33018beeac699cae305f
mvajhi <mvajhimv@gmail.com>
2025-03-18 15:03:51 +0330

    Feat: Added addBook to spring

--- CA3/src/main/java/org/example/controller/BookShopController.java
+++ CA3/src/main/java/org/example/controller/BookShopController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.request.AddAuthorRequest;
+import org.example.request.AddBookRequest;
 import org.example.request.AddUserRequest;
 import org.example.services.BookShopService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class BookShopController {
 
-    BookShopService bookShopService = new BookShopService();
+    @Autowired
+    BookShopService bookShopService;
 
     @RequestMapping("/user")
     @PostMapping
@@ -23,5 +25,11 @@ public class BookShopController {
     @PostMapping
     public String addAuthor(@RequestBody AddAuthorRequest request) {
         return bookShopService.addAuthor(request).convertToString();
+    }
+
+    @RequestMapping("/book")
+    @PostMapping
+    public String addBook(@RequestBody AddBookRequest request) {
+        return bookShopService.addBook(request).convertToString();
     }
 }
