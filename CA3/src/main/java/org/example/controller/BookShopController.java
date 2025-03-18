a24b44bff8a045852a9293623655586f4736351c
mvajhi <mvajhimv@gmail.com>
2025-03-18 14:34:14 +0330

    Feat: Added author to spring

--- CA3/src/main/java/org/example/controller/BookShopController.java
+++ CA3/src/main/java/org/example/controller/BookShopController.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.request.AddAuthorRequest;
 import org.example.request.AddUserRequest;
 import org.example.services.BookShopService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +17,11 @@ public class BookShopController {
     @PostMapping
     public String addUser(@RequestBody AddUserRequest request) {
         return bookShopService.addUser(request).convertToString();
+    }
+
+    @RequestMapping("/author")
+    @PostMapping
+    public String addAuthor(@RequestBody AddAuthorRequest request) {
+        return bookShopService.addAuthor(request).convertToString();
     }
 }
