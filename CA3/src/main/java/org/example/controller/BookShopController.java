68417eed120d65aa1c4b85f65a1f471952d03ac8
mvajhi <mvajhimv@gmail.com>
2025-03-19 12:04:24 +0330

    Fix: change request to /api

--- CA3/src/main/java/org/example/controller/BookShopController.java
+++ CA3/src/main/java/org/example/controller/BookShopController.java
@@ -6,16 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class BookShopController {
 
     @Autowired
     BookShopService bookShopService;
-
-    @RequestMapping("/user")
-    @PostMapping
-    public String addUser(@RequestBody AddUserRequest request) {
-        return bookShopService.addUser(request).convertToString();
-    }
 
     @RequestMapping("/author")
     @PostMapping
