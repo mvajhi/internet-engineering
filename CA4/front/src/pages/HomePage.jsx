fbaecc624adea660b2ecdc4dda156432e1e41623
Ali <you@example.com>
2025-04-20 09:22:19 +0330

    Fix: Edit class in homepage

--- CA4/front/src/pages/HomePage.jsx
+++ CA4/front/src/pages/HomePage.jsx
@@ -45,31 +45,31 @@ const HomePage = () => {
 
 
             <br/><br/>
-    <div class="p-4 rounded-3 d-flex flex-column flex-md-row align-items-center w-100 hello-box">
-        <div class="container py-5 px-4">
-            <div class="p-4 rounded-3 d-flex flex-column flex-md-row align-items-center w-60">
-                <div class="text-center text-md-start col-md-8 px-4 ps-md-5">
-                    <p class="mb-3 fs-4 ps-md-5">
+    <div className="p-4 rounded-3 d-flex flex-column flex-md-row align-items-center w-100 hello-box">
+        <div className="container py-5 px-4">
+            <div className="p-4 rounded-3 d-flex flex-column flex-md-row align-items-center w-60">
+                <div className="text-center text-md-start col-md-8 px-4 ps-md-5">
+                    <p className="mb-3 fs-4 ps-md-5">
                         Welcome to MioBook – the online bookstore where you can buy or borrow books with ease.
                     </p>
-                    <p class="mb-3 fs-4 ps-md-5">
+                    <p className="mb-3 fs-4 ps-md-5">
                         Whether you're looking for the latest bestseller, a classic novel, or a niche title, MioBook has
                         you
                         covered.
                     </p>
-                    <p class="mb-3 fs-4 ps-md-5">
+                    <p className="mb-3 fs-4 ps-md-5">
                         Here, you can quickly find books by title, author, and genre. And if you’re not sure to buy? Try
                         borrowing instead! Rent a book for just a fraction of the price and enjoy full access for a set
                         period.
                     </p>
-                    <p class="mb-3 fs-4 ps-md-5">
+                    <p className="mb-3 fs-4 ps-md-5">
                         Your next great read is just a click away. Visit MioBook today and let the perfect book find
                         you!
                         📚✨
                     </p>
                 </div>
-                <div class="col-md-4 mt-4 mt-md-0 text-end pe-md-5">
-                    <img src="/assets/shelf.svg" class="img-fluid pe-md-2" alt=""/>
+                <div className="col-md-4 mt-4 mt-md-0 text-end pe-md-5">
+                    <img src="/assets/shelf.svg" className="img-fluid pe-md-2" alt=""/>
                 </div>
             </div>
         </div>
