61d1711743fd24129aa99f151e3d0357f4ae3eba
mvajhi <mvajhimv@gmail.com>
2025-04-20 10:51:44 +0330

    Feat: added user icon

--- CA4/front/src/components/Header.jsx
+++ CA4/front/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
+import { useNavigate, Link } from 'react-router-dom';
+
 
 const Header = ({ searchQuery, onSearchChange, onSearchSubmit }) => {
     return (
@@ -12,7 +13,7 @@ const Header = ({ searchQuery, onSearchChange, onSearchSubmit }) => {
                     <div className="d-none d-md-flex bg-light rounded w-50">
                         <SearchBar />
                     </div>
-                    <button className="btn btn-green-custom text-white">Buy now</button>
+                    <ProfileIcon userName="John Doe" />
                 </div>
 
             </header>
@@ -20,6 +21,54 @@ const Header = ({ searchQuery, onSearchChange, onSearchSubmit }) => {
                 <SearchBar />
             </div>
         </>
+    );
+};
+
+const ProfileIcon = ({ userName }) => {
+    const firstLetter = userName.charAt(0).toUpperCase();
+    return (
+        <div className="dropdown">
+            {/* آیکون پروفایل */}
+            <button
+                className="btn btn-success rounded-circle d-flex justify-content-center align-items-center  px-3 py-2 user-select-none"
+                type="button"
+                id="profileDropdown"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+            >
+                {firstLetter}
+            </button>
+
+            {/* منوی کشویی */}
+            <ul
+                className="dropdown-menu dropdown-menu-end bg-select-bar rounded-3 mt-1"
+                aria-labelledby="profileDropdown"
+            >
+                <li className="dropdown-header fw-bold text-dark fs-6">{userName}</li>
+                <li>
+                    <hr className="dropdown-divider mx-3 bg-dark" />
+                </li>
+                {/* TODO : Add links to the pages */}
+                <DropdownItem href="#profile" iconSrc="/assets/profile_menu/person.svg" text="Profile" />
+                <DropdownItem href="#my-books" iconSrc="/assets/profile_menu/book.svg" text="My Books" />
+                <DropdownItem href="#buy-cart" iconSrc="/assets/profile_menu/cart.svg" text="Buy Cart" />
+                <DropdownItem href="#purchase-history" iconSrc="/assets/profile_menu/history.svg" text="Purchase History" />
+                <li>
+                    <hr className="dropdown-divider mx-3" />
+                </li>
+                <DropdownItem href="#logout" iconSrc="/assets/profile_menu/logout.svg" text="Logout" />
+            </ul>
+        </div>
+    );
+};
+
+const DropdownItem = ({ href, iconSrc, text }) => {
+    return (
+        <li>
+            <Link className="dropdown-item d-flex align-items-center pb-2" to={href}>
+                <img src={iconSrc} className="me-2" alt="" /> {text}
+            </Link>
+        </li>
     );
 };
 
@@ -31,7 +80,7 @@ const SearchBar = () => {
     };
 
     return (
-        <div className="d-flex bg-light-custom rounded p-2 w-100 text-secondary">
+        <div className="d-flex bg-light-custom rounded p-1 w-100 text-secondary">
             <div className="dropdown">
                 <button
                     className="btn bg-light-custom dropdown-toggle border-0"
@@ -41,7 +90,7 @@ const SearchBar = () => {
                 >
                     {selected}
                 </button>
-                <ul className="dropdown-menu bg-light">
+                <ul className="dropdown-menu bg-select-bar rounded-3 border-1 mt-2">
                     <li>
                         <button
                             className="dropdown-item"
