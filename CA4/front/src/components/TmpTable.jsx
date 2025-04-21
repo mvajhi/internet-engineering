b945dc0a3cf6bf44b5a91f49fbae05355943b6a5
mvajhi <mvajhimv@gmail.com>
2025-04-21 20:49:55 +0330

    Feat: handel tmp table

---
+++ CA4/front/src/components/TmpTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TmpTable = (img) => {
+    return (
+        <div class="text-center">
+            <img src={img} class="img-fluid" alt="No product" />
+        </div>
+    );
+};
