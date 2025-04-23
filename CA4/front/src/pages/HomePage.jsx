6aaeea50babfebe34f3b0bbc8e44ba26111aec9d
mvajhi <mvajhimv@gmail.com>
2025-04-24 00:38:14 +0330

    Style: add background color to NewReleases section and HomePage container

--- CA4/front/src/pages/HomePage.jsx
+++ CA4/front/src/pages/HomePage.jsx
@@ -73,7 +73,7 @@ const HomePage = () => {
                 </div>
             </div>
 
-            <div className="container py-5">
+            <div className="container py-5 bg-light">
 
                 <NewReleases />
                 <TopRatedBooks />
